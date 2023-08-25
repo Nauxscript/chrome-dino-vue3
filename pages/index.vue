989388b6b31a492e5c2159396aa82e64ac47362b
nauxscript <nauxscript@gmail.com>
2023-08-25 18:16:14 +0800

    chore: remove unneeded code

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,9 @@ const online = useOnline()
 
 <template>
   <div>
-    <Logos mb-6 />
     <Suspense>
       <ClientOnly>
-        <PageView v-if="online" />
+        <Dino v-if="online" />
         <div v-else text-gray:80>
           You're offline
         </div>
@@ -18,6 +17,5 @@ const online = useOnline()
         </div>
       </template>
     </Suspense>
-    <InputEntry />
   </div>
 </template>
