d66cca657c71b76af8a06188153d1a310c02e73c
nauxscript <nauxscript@gmail.com>
2023-08-27 11:47:23 +0800

    feat: dino runing

---
+++ components/Dino.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="dino" />
+</template>
+
+<style scoped>
+.dino {
+  width: 88px;
+  height: 100px;
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAhAAAABeAgMAAAAPo8UvAAAADFBMVEX///9TU1P39/f///+TS9URAAAAAXRSTlMAQObYZgAAASdJREFUeF7t1qFOBEEQRdEyGP7vGQy/hsHc/0MPSe8ylU2vKEIqqQnviRZXdI7pyUQuONda901FGAG6j8aa+6mDEUboHP01sk5EHHWEjt/UY0dk/U+Ir/cdkXUEovV1GFF/HQMR/mLWEUYYYQRrf65XRhgB2595Y80lYRjCCG7AV/IZ0FdDabgDhiKMgE+tAX01ES+ajDBCADpHZw0tRdaZCCNEGhCdNSSlQTEVYUROQGeNxxoxH2EErXU+wohdQXONqyBorDsixiB2Be01JiOM2BXQX1MRUxFGpAL6aypiMsIIJCFBtSK98fFYKd6wFDEbYUQgEYh6hTSkonbDDTAdYQTrKNd9QPWGUFwAYYRYR7U+XemGfB0ajTACWEe1Pl3thtxMhBHfOCEbEnR2KZcAAAAASUVORK5CYII=);
+  background-repeat: no-repeat;
+  animation: dino-run .2s steps(1) infinite;
+  transition: none;
+}
+
+@keyframes dino-run {
+  0% {
+    background-position: 0 0;
+  }
+  33% {
+    background-position: -176px 0;
+  }
+
+  66% {
+    background-position: -264px 0;
+  }
+}
+</style>
