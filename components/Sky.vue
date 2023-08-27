1651f304867bf4d795e2d37a6a2fe79dc995ed4b
nauxscript <nauxscript@gmail.com>
2023-08-27 14:49:16 +0800

    refactor: sky

---
+++ components/Sky.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import Cloud from './Cloud.vue'
+
+const cloud_1 = ref<InstanceType<typeof Cloud>>()
+
+function checkCloud() {
+  requestAnimationFrame(() => {
+    if (cloud_1.value?.isStop())
+      cloud_1.value.restart()
+
+    checkCloud()
+  })
+}
+
+onMounted(() => {
+  checkCloud()
+})
+</script>
+
+<template>
+  <div class="sky">
+    <Cloud ref="cloud_1" />
+  </div>
+</template>
+
+<style scoped>
+.sky {
+  height: 120px;
+  margin-bottom: -120px;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+</style>
