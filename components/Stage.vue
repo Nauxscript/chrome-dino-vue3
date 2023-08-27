9363b16d79d1fbb539e1ff2cc3e90935c6002540
nauxscript <nauxscript@gmail.com>
2023-08-27 15:22:02 +0800

    feat: stage pause

--- components/Stage.vue
+++ components/Stage.vue
@@ -1,14 +1,36 @@
 <script lang="ts" setup>
+const stageRef = ref<HTMLDivElement>()
 
+function handlePause() {
+  if (!stageRef.value)
+    return
+  stageRef.value.classList.add('pause')
+}
+
+function handlePlay() {
+  if (!stageRef.value)
+    return
+  stageRef.value.classList.remove('pause')
+}
 </script>
 
 <template>
-  <div class="stage">
+  <div ref="stageRef" class="stage">
     <Sky />
     <Dino />
     <Ground />
+    <!-- <button @click="handlePause">
+      stop
+    </button>
+    <button @click="handlePlay">
+      run
+    </button> -->
   </div>
 </template>
 
-<style scoped>
+<style>
+.pause div {
+  animation-play-state: paused !important;
+  transition: none !important;
+}
 </style>
